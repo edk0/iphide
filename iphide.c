37a4c500cbf0de065dcd1fec8739a7ce1b65822e
Ed Kellett <e@kellett.im>
2017-11-27 02:09:44 +0000

    add more privacy for the initial part of an IP
    Implement (rudimentary, homegrown, not crypto-safe) format-preserving
    encryption for the beginning of an IP. The bits covered by this second
    algorithm do not preserve structure. It is a permutation, though, so
    collisions are not a problem.

--- iphide.c
+++ iphide.c
@@ -13,6 +13,10 @@ static const uint8_t key[16] = {
 	59, 61, 67, 71, 73, 79, 83, 89,
 };
 
+#define SKIP_PREFIX 12
+
+#define FEISTEL_ROUNDS 4
+
 
 static void addbit(void *in, void *out, size_t dstlen, size_t bit) {
 	uint8_t hash[8];
@@ -29,8 +33,79 @@ static void addbit(void *in, void *out, size_t dstlen, size_t bit) {
 }
 
 
+static void bitcopy(void *dst_, size_t dstbit, void *src_, size_t srcbit, size_t n)
+{
+#	define READ_BITS(nb_) do {\
+		size_t nb = (nb_); \
+		size_t p = nb < 8 - srcbit ? nb : 8 - srcbit; \
+		c = src[0] << (srcbit); \
+		if (p < nb) c |= (src[1] & 0xff << (8 - nb + p)) >> p; \
+	} while (0)
+
+	uint8_t c;
+	uint8_t *dst = dst_, *src = src_;
+
+	dst += dstbit / 8;
+	dstbit = dstbit % 8;
+	src += srcbit / 8;
+	srcbit = srcbit % 8;
+
+	/* set dst[dstbit..] from src */
+	if (dstbit) {
+		READ_BITS(8 - dstbit);
+		*dst |= c >> dstbit;
+		++dst;
+		n -= 8 - dstbit;
+		srcbit += 8 - dstbit;
+		if (srcbit >= 8) {
+			srcbit -= 8;
+			src++;
+		}
+		dstbit = 0;
+	}
+
+	/* set dst from src */
+	while (n >= 8) {
+		READ_BITS(8);
+		*dst++ = c;
+		src++;
+		n -= 8;
+	}
+
+	/* trail */
+	if (n) {
+		READ_BITS(n);
+		*dst |= c;
+	}
+#	undef READ_BITS
+}
+
+
+static void permute(void *in, void *out, size_t bits) {
+	uint8_t bl[8] = {0}, br[8] = {0};
+	uint8_t *l = bl, *r = br, *tmp;
+	uint8_t hash[8];
+
+	bitcopy(bl, 0, in, 0, (bits - 1) / 2 + 1);
+	bitcopy(br, 0, in, (bits - 1) / 2 + 1, bits / 2);
+
+	for (int i = 0; i < FEISTEL_ROUNDS; i++) {
+		size_t trim = (l == bl) ? (bits - 1) / 2 + 1 : bits / 2;
+		siphash(r, 8, key, hash, sizeof hash);
+		for (int j = 0; j < (trim - 1) / 8 + 1; j++)
+			l[j] ^= hash[j];
+		if (trim % 8)
+			l[trim/8] &= 0xff << (8 - trim % 8);
+		tmp = l; l = r; r = tmp;
+	}
+
+	bitcopy(out, 0, bl, 0, (bits - 1) / 2 + 1);
+	bitcopy(out, (bits - 1) / 2 + 1, br, 0, bits / 2);
+}
+
+
 int main(int argc, char **argv) {
-	char b[200];
+	char b[200] = {0};
 
 	union {
 		struct in_addr  ip4;
@@ -49,12 +124,13 @@ int main(int argc, char **argv) {
 		return -1;
 	}
 
-	for (int i = 0; i < 32; i++) {
+	permute(&addr.ip4, &dst.ip4, SKIP_PREFIX);
+	for (int i = SKIP_PREFIX; i < 32; i++) {
 		addbit(&addr.ip4, &dst.ip4, 4, i);
 	}
 
 	inet_ntop(AF_INET, &dst.ip4, b, sizeof b);
-	puts(b);
+	printf("%s -> %s\n", argv[1], b);
 
 	return 0;
 }
